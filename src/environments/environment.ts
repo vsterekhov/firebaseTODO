50ff0d89cd580f9f24e9ca15f7ca8c6f0d7b7a7c
vsterekhov <vsterekhov88@gmail.com>
2019-08-15 11:51:44 +0700

    Добавил Firebase config в environments. Добавил необходимые модули в AppModule.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDB3-ttSwIQQa0xXIBbB4qx6qX2Bo1TsAI',
+    authDomain: 'fir-todo-ee13e.firebaseapp.com',
+    databaseURL: 'https://fir-todo-ee13e.firebaseio.com',
+    projectId: 'fir-todo-ee13e',
+    storageBucket: '',
+    messagingSenderId: '231482879994',
+    appId: '1:231482879994:web:ca133e702b456354'
+  }
 };
 
 /*
