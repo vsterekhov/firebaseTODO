38b4e0b277b1aecb23d417be5e8d77667b05512e
vsterekhov <vsterekhov88@gmail.com>
2019-08-21 10:04:26 +0700

    Добавлена папка prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDB3-ttSwIQQa0xXIBbB4qx6qX2Bo1TsAI',
+    authDomain: 'fir-todo-ee13e.firebaseapp.com',
+    databaseURL: 'https://fir-todo-ee13e.firebaseio.com',
+    projectId: 'fir-todo-ee13e',
+    storageBucket: '',
+    messagingSenderId: '231482879994',
+    appId: '1:231482879994:web:ca133e702b456354'
+  }
 };
