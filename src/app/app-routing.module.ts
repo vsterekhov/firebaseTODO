f6019273789a0c17a8afdfc9b00323d405ad8e36
vsterekhov <vsterekhov88@gmail.com>
2019-08-21 11:43:47 +0700

    run build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
 
 const routes: Routes = [
   { path: '',  component: TodoListComponent },
-  { path: ':listKey',  component: TodoListComponent },
+  { path: '#:listKey',  component: TodoListComponent },
   { path: '**',  redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
